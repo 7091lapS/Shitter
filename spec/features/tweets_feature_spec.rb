58096321812435c00d822aad9f95d7cc91f3c056
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-16 16:15:29 +0100

    add tweets routes, controller and view

---
+++ spec/features/tweets_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'tweets' do
+  context 'no tweets have been added' do
+    scenario 'a no-tweets warning is displayed' do
+      visit tweets_path
+      expect(page).to have_content 'No tweets yet!'
+    end
+  end
+end
