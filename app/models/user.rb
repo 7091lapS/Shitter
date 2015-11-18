5d7ae3a98ae42ca7c6133ef39c3882b02b93a725
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 19:05:25 +0100

    add passive relationship for followers and helper method followed_by?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id", dependent: :destroy
 
-  # I overrid the default "followed" with "following", because "followeds" in english sounds a bit wrong
+  # I overrode the default "followed" with "following", because "followeds" in english sounds a bit wrong
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -20,4 +23,9 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following.include?(other_user)
   end
+
+  def followed_by?(other_user)
+    followers.include?(other_user)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,15 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many :tweets}
   it { is_expected.to have_many :active_relationships}
   it { is_expected.to have_many :following}
+  it { is_expected.to have_many :passive_relationships}
+  it { is_expected.to have_many :followers}
 
   it "should follow and unfollow users" do
     bernie = create(:user)
     hillary = create(:user)
     expect(bernie.following?(hillary)).to eq(false)
     bernie.follow(hillary)
+    expect(hillary.followed_by?(bernie)).to eq(true)
     expect(bernie.following?(hillary)).to eq(true)
     bernie.unfollow(hillary)
     expect(bernie.following?(hillary)).to eq(false)
