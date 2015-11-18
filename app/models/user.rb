03c8203e27216a5a3e0befc02d43420aaeeb6ae1
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 18:15:01 +0100

    add tests for user follow helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id", dependent: :destroy
 
+  # I overrid the default "followed" with "following", because "followeds" in english sounds a bit wrong  
+  has_many :following, through: :active_relationships, source: :followed
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to have_many :tweets}
   it { is_expected.to have_many :active_relationships}
+  it { is_expected.to have_many :following}
+
+  it "should follow and unfollow users" do
+    bernie = create(:user)
+    hillary = create(:user)
+    expect(bernie.following?(hillary)).to eq(false)
+    bernie.follow(hillary)
+    expect(bernie.following?(hillary)).to eq(true)
+    bernie.unfollow(hillary)
+    expect(bernie.following?(hillary)).to eq(false)
+  end
 end
