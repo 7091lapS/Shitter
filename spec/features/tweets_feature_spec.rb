1047dc08f73d7e246c6a54975a71acbf8a4074ee
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 16:08:05 +0100

    add test for not displaying unfollowed users

--- spec/features/tweets_feature_spec.rb
+++ spec/features/tweets_feature_spec.rb
@@ -11,16 +11,36 @@ feature 'tweets' do
 
   context 'creating tweets' do
     before do
-      visit tweets_path
       @user = create(:user)
+      visit tweets_path
       sign_in(@user)
     end
 
-    scenario 'a tweet can be created' do
+    scenario 'a tweet can be created and is displayed' do
       visit tweets_path
       fill_in "tweet[tweet_content]", with: 'test tweet'
       click_button 'Post message'
       expect(page).to have_content "#{@user.email}: test tweet"
+      click_link 'Sign out'
+      expect(page).to have_content "#{@user.email}: test tweet"
     end
+  end
+
+  context 'signed in without any followed users' do
+    before do
+      user_1 = create(:user)
+      @user_2 = create(:user)
+      visit tweets_path
+      sign_in(user_1)
+    end
+
+    scenario 'a user cannot see posts by unfollowed users' do
+      fill_in "tweet[tweet_content]", with: 'test tweet by user_1'
+      click_button 'Post message'
+      click_link 'Sign out'
+      sign_in(@user_2)
+      expect(page).not_to have_content('test tweet by user_1')
+    end
+    
   end
 end
