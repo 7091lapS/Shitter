de5a5237a01a1e4326368f7d49296705134db260
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 18:35:26 +0100

    add user follow helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,21 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id", dependent: :destroy
 
-  # I overrid the default "followed" with "following", because "followeds" in english sounds a bit wrong  
+  # I overrid the default "followed" with "following", because "followeds" in english sounds a bit wrong
   has_many :following, through: :active_relationships, source: :followed
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
