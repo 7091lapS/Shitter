d4498ec65efdcf1833019269e590cbbf16feb5f2
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 17:44:13 +0100

    add and test active association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id", dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {is_expected.to belong_to :follower}
+  it {is_expected.to belong_to :followed}
 end
