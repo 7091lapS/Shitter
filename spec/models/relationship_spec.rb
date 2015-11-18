358fd90d2024aac9f5067e78dd8f868dc7d03d14
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-18 17:50:17 +0100

    add and test validation of Relationship's properties

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Relationship, type: :model do
   it {is_expected.to belong_to :follower}
   it {is_expected.to belong_to :followed}
+  it {is_expected.to validate_presence_of :follower_id}
+  it {is_expected.to validate_presence_of :followed_id}
 end
