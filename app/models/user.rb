780610df32b9e4f8b54ae99c428983b7302ceb2f
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-21 20:17:50 +0100

    update authentication with username in devise, update devise views for login, fix helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,28 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
+  validates :username, :presence => true, :uniqueness => {
+    :case_sensitive => false }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
--- spec/helpers/sign_in_helper.rb
+++ spec/helpers/sign_in_helper.rb
@@ -1,6 +1,6 @@
 def sign_in(user)
   click_link('Sign in')
-  fill_in('Email', with: user.email)
+  fill_in('Login', with: user.username)
   fill_in('Password', with: user.password)
   click_button('Log in')
 end
