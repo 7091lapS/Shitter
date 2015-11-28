2585e8bbd540722c201afdb08d76e1bd0d66f88f
Antonio Voce <antonio.v0c3@gmail.com>
2015-11-28 16:27:05 +0100

    fix tests for following users

--- spec/features/followers_feature_spec.rb
+++ spec/features/followers_feature_spec.rb
@@ -4,10 +4,10 @@ feature 'following and unfollowing users' do
 
   context 'user signed in' do
     before do
-      user_1 = create(:user)
+      @user_1 = create(:user)
       @user_2 = create(:user)
       visit tweets_path
-      sign_in(user_1)
+      sign_in(@user_1)
       fill_in "tweet[tweet_content]", with: 'test tweet by user_1'
       click_button 'Post message'
       click_link 'Sign out'
@@ -21,31 +21,25 @@ feature 'following and unfollowing users' do
       expect(page).to have_content 'No tweets yet!'
     end
 
-    # scenario 'can see a link to follow another user' do
-    #   within('ul') do
-    #     expect(page).to have_link 'Follow'
-    #   end
-    # end
-
     scenario 'after following, should see the tweet in the Following stream' do
-      click_link "#{@user_2.username}"
-      click_link 'Follow'
+      click_link "#{@user_1.username}"
+      click_button 'Follow'
       click_link 'Following Stream'
       expect(page).to have_content 'test tweet by user_1'
     end
 
     scenario 'after following, should see Unfollow link' do
-      click_link "#{@user_2.username}"
-      click_link 'Follow'
-      expect(page).to have_link 'Unfollow'
-      click_link 'Unfollow'
-      expect(page).to have_link 'Follow'
+      click_link "#{@user_1.username}"
+      click_button 'Follow'
+      expect(page).to have_button 'Unfollow'
+      click_button 'Unfollow'
+      expect(page).to have_button 'Follow'
     end
 
     scenario 'after unfollowing, should not see the post in the Following stream' do
-      click_link "#{@user_2.username}"
-      click_link 'Follow'
-      click_link 'Unfollow'
+      click_link "#{@user_1.username}"
+      click_button 'Follow'
+      click_button 'Unfollow'
       click_link 'Following Stream'
       expect(page).not_to have_content 'test tweet by user_1'
     end
@@ -77,8 +71,8 @@ feature 'following and unfollowing users' do
 
     scenario 'a guest cannot see follow/unfollow links' do
       visit tweets_path
-      expect(page).not_to have_link 'Follow'
-      expect(page).not_to have_link 'Unfollow'
+      expect(page).not_to have_button 'Follow'
+      expect(page).not_to have_button 'Unfollow'
     end
   end
 end
